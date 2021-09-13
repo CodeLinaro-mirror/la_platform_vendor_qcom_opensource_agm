602095e8dcd2c3ebbb025e57580261a2ffcd788e
Rohit kumar <rohitkr@codeaurora.org>
2021-09-13 09:49:42 +0530

    plugins: tinyalsa: Fix null pointer dereference
    Change-Id: Icd9d36171b13b8cd1dd03004ba14e16e9729f601

--- plugins/tinyalsa/test/agmplay.c
+++ plugins/tinyalsa/test/agmplay.c
@@ -145,8 +145,7 @@ int main(int argc, char **argv)
             argv++;
             if (*argv)
                 intf_name = *argv;
-        }
-       if (strcmp(*argv, "-h") == 0) {
+        } else if (strcmp(*argv, "-h") == 0) {
             argv++;
             if (*argv)
                 haptics = *argv;
