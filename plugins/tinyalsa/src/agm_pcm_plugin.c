2bcc48405b52ce8d061e0a22bf63e95cd7702854
Hao Zhang <quic_hzhan@quicinc.com>
2022-01-20 21:41:58 +0800

    upmerge from 889f093a2e4b8a394250b41bc186aa01e3696c41
    Change-Id: I678e35fb737a6b9b6e600a73da1fe49823f08799

--- plugins/tinyalsa/src/agm_pcm_plugin.c
+++ plugins/tinyalsa/src/agm_pcm_plugin.c
@@ -637,7 +637,7 @@ static snd_pcm_sframes_t agm_pcm_get_avail(struct pcm_plugin *plugin)
         else if ((snd_pcm_uframes_t)avail >= priv->pos_buf->boundary)
             avail -= priv->pos_buf->boundary;
     } else if (plugin->mode & PCM_IN) {
-        __builtin_sub_overflow(priv->pos_buf->hw_ptr, priv->pos_buf->appl_ptr, &avail);
+        avail = priv->pos_buf->hw_ptr - priv->pos_buf->appl_ptr;
         if (avail < 0)
             avail += priv->pos_buf->boundary;
     }
