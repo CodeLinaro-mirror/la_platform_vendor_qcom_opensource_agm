af50467a377073f332d7bdd71d073a8efbc90173
xsang <quic_xsang@quicinc.com>
2022-03-14 20:02:48 +0530

    agm: lock device object before metadata merge
    Add a missing lock for device object before
    doing a metadata merge.
    
    Change-Id: Ie02607aacd5bacc1bbbc46218983427c96beb832

--- service/src/session_obj.c
+++ service/src/session_obj.c
@@ -1440,8 +1440,10 @@ int session_obj_rw_acdb_params_with_tag(
             goto error;
         }
 
+        pthread_mutex_lock(&aif_obj->dev_obj->lock);
         merged_metadata = metadata_merge(3, &sess_obj->sess_meta,
                           &aif_obj->sess_aif_meta, &aif_obj->dev_obj->metadata);
+        pthread_mutex_unlock(&aif_obj->dev_obj->lock);
     }
 
     if (!merged_metadata) {
