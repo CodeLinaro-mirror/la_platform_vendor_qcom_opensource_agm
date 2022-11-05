07f5445592834bf48b851b98691a1f33e7bcb920
Kogara Naveen Kumar <quic_nkogara@quicinc.com>
2022-11-05 14:23:04 +0530

    agm: Use gsl_fe header for msmnile_gvmgh instead of gsl
    Suggested-by: Prashant Andani
    
    Change-Id: I3abfdff4c9efbd0766268106dc9bb2cc5706f1a9

--- service/Android.mk
+++ service/Android.mk
@@ -22,7 +22,7 @@ LOCAL_CFLAGS        += -DACDB_DELTA_FILE_PATH="/data/vendor/audio/acdbdata/delta
 LOCAL_C_INCLUDES    := $(LOCAL_PATH)/inc/public
 LOCAL_C_INCLUDES    += $(LOCAL_PATH)/inc/private
 
-ifeq ($(PRODUCT_NAME), msmnile_gvmq)
+ifneq (,$(filter $(PRODUCT_NAME), msmnile_gvmq msmnile_gvmgh))
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/include/mm-audio/gsl_fe
 else
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/include/mm-audio/ar/gsl
