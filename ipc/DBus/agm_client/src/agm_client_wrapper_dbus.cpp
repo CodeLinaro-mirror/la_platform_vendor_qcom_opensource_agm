c71bc41e590bfe23a2be350e5156641ad32a3975
Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
2023-04-14 16:35:47 +0530

    agm: ipc: Add compress playback functions
    Add compress playback ipc function
    
    Change-Id: I7fd93c5d7687e5f80bae01c8ffe110c7dfb22c39

--- ipc/DBus/agm_client/src/agm_client_wrapper_dbus.cpp
+++ ipc/DBus/agm_client/src/agm_client_wrapper_dbus.cpp
@@ -1220,7 +1220,7 @@ int agm_session_get_buf_info(uint32_t session_id, struct agm_buf_info *buf_info,
 
     if(!buf_info) {
         AGM_LOGE("%s: buf_info is NULL\n", __func__);
-	return -EINVAL;
+        return -EINVAL;
     }
 
     value_1 = g_variant_new_uint32(session_id);
@@ -1260,6 +1260,41 @@ int agm_session_get_buf_info(uint32_t session_id, struct agm_buf_info *buf_info,
     g_variant_iter_next(&struct_i, "i", &buf_info->pos_buf_fd);
     g_variant_iter_next(&struct_i, "i", &buf_info->pos_buf_size);
     g_variant_unref(result);
+    return rc;
+}
+
+int agm_set_gapless_session_metadata(uint64_t handle,
+                         enum agm_gapless_silence_type type,
+                         uint32_t silence)
+{
+    GVariant *argument = NULL;
+    GVariant *result = NULL;
+    GError *error = NULL;
+    agm_client_session_data *ses_data = (agm_client_session_data *) handle;
+    int rc = 0;
+
+    g_assert(ses_data != NULL);
+    g_assert(ses_data->proxy != NULL);
+
+    AGM_LOGD("%s\n", __func__);
+
+    argument = g_variant_new("(@u@u)", g_variant_new_uint32(type), g_variant_new_uint32(silence));
+
+    result = g_dbus_proxy_call_sync(ses_data->proxy,
+                                    "AgmSetGaplessSessionMetadata",
+                                    argument,
+                                    G_DBUS_CALL_FLAGS_NONE,
+                                    -1,
+                                    NULL,
+                                    &error);
+
+    if (result == NULL) {
+        AGM_LOGE("%s: Error invoking AgmSetGaplessSessionMetadata: %s\n", __func__,
+                  error->message);
+        g_error_free(error);
+        return -EINVAL;
+    }
+
     return rc;
 }
 
@@ -1811,11 +1846,14 @@ int agm_session_write(uint64_t handle, void *buf, size_t *byte_count) {
         return -EINVAL;
     }
 
-    start_time = g_get_monotonic_time();
-    end_time = start_time + (AGM_DBUS_ASYNC_CALL_TIMEOUT_MS * G_TIME_SPAN_MILLISECOND);
-    g_cond_wait_until(&ses_data->cond, &ses_data->mutex, end_time);
-    if (g_get_monotonic_time() >= end_time){
-        AGM_LOGE("%s: -ETIMEDOUT %d\n", __func__, g_get_monotonic_time());
+    if (ses_data->session_id != 105)
+    {
+        start_time = g_get_monotonic_time();
+        end_time = start_time + (AGM_DBUS_ASYNC_CALL_TIMEOUT_MS * G_TIME_SPAN_MILLISECOND);
+        g_cond_wait_until(&ses_data->cond, &ses_data->mutex, end_time);
+        if (g_get_monotonic_time() >= end_time){
+            AGM_LOGE("%s: -ETIMEDOUT %d\n", __func__, g_get_monotonic_time());
+        }
     }
 
     g_mutex_unlock(&ses_data->mutex);
--- ipc/DBus/agm_server/src/agm_server_wrapper_dbus.cpp
+++ ipc/DBus/agm_server/src/agm_server_wrapper_dbus.cpp
@@ -145,6 +145,7 @@ enum AgmModuleMethods {
     AgmSessionGetBufInfo,
     AgmGetBufferTimestamp,
     AgmSessionOpen,
+    AgmSetGaplessSessionMetadata,
     AgmDbusModuleMethodMax
 };
 
@@ -234,6 +235,9 @@ static void ipc_agm_session_get_params(DBusConnection *conn,
 static void ipc_agm_session_get_buf_info(DBusConnection *conn,
                                        DBusMessage *msg,
                                        void *userdata);
+static void ipc_agm_set_gapless_session_metadata(DBusConnection *conn,
+                                       DBusMessage *msg,
+                                       void *userdata);
 static void ipc_agm_session_open(DBusConnection *conn,
                                  DBusMessage *msg,
                                  void *userdata);
@@ -312,7 +316,8 @@ static agm_dbus_method agm_dbus_module_methods[AgmDbusModuleMethodMax] = {
     {"AgmSessionGetParams", "uuay", ipc_agm_session_get_params},
     {"AgmSessionGetBufInfo", "uu", ipc_agm_session_get_buf_info},
     {"AgmGetBufferTimestamp", "u", ipc_agm_get_buffer_timestamp},
-    {"AgmSessionOpen", "uu", ipc_agm_session_open}
+    {"AgmSessionOpen", "uu", ipc_agm_session_open},
+    {"AgmSetGaplessSessionMetadata", "uu", ipc_agm_set_gapless_session_metadata}
 };
 
 static agm_dbus_method agm_dbus_session_methods[AgmDbusSessionMethodMax] = {
@@ -1048,6 +1053,57 @@ static void ipc_agm_session_get_buf_info(DBusConnection *conn,
 
 exit:
     return;
+}
+
+
+static void ipc_agm_set_gapless_session_metadata(DBusConnection *conn,
+                                       DBusMessage *msg,
+                                       void *userdata) {
+    DBusMessage *reply = NULL;
+    DBusMessageIter arg_i;
+    uint32_t type, silence;
+    agm_session_data *ses_data = (agm_session_data *)userdata;
+
+    if (userdata == NULL) {
+        AGM_LOGE("Invalid userdata");
+        agm_dbus_send_error(mdata->conn, msg, DBUS_ERROR_FAILED,
+                            "userdata is NULL");
+        return;
+    }
+
+    if (!dbus_message_iter_init(msg, &arg_i)) {
+        AGM_LOGE("ipc_agm_set_gapless_session_metadata has no arguments");
+        agm_dbus_send_error(mdata->conn, msg, DBUS_ERROR_FAILED,
+                            "ipc_agm_set_gapless_session_metadata has no arguments");
+        return;
+    }
+
+    if (strcmp(dbus_message_get_signature(msg), "uu")) {
+        AGM_LOGE("Invalid signature for ipc_agm_set_gapless_session_metadata.");
+        agm_dbus_send_error(mdata->conn, msg, DBUS_ERROR_FAILED,
+                           "Invalid signature for ipc_agm_set_gapless_session_metadata.");
+        return;
+    }
+
+    AGM_LOGV("%s : ", __func__);
+
+    dbus_message_iter_get_basic(&arg_i, &type);
+    dbus_message_iter_next(&arg_i);
+    dbus_message_iter_get_basic(&arg_i, &silence);
+
+    if (agm_set_gapless_session_metadata(ses_data->handle,
+                                              (enum agm_gapless_silence_type)type,
+                                              silence) != 0) {
+        AGM_LOGE("%s :agm_set_gapless_session_metadata failed.", __func__);
+        agm_dbus_send_error(mdata->conn, msg, DBUS_ERROR_FAILED,
+                            "agm_set_gapless_session_metadata failed.");
+        return;
+    }
+
+    reply = dbus_message_new_method_return(msg);
+    dbus_connection_send(conn, reply, NULL);
+    dbus_message_unref(reply);
+
 }
 
 static void ipc_agm_session_aif_set_cal(DBusConnection *conn,
@@ -2288,18 +2344,29 @@ static void ipc_agm_session_write(DBusConnection *conn,
     dbus_message_iter_recurse(&arg_i, &array_i);
     dbus_message_iter_get_fixed_array(&array_i, addr_value, &n_elements);
 
-    pthread_mutex_lock(&ses_data->lock);
-    ses_data->buf_size = buf_size;
-    if (ses_data->thread_state != SES_THREAD_IDLE) {
+    if (ses_data->session_id != 105)
+    {
+        pthread_mutex_lock(&ses_data->lock);
+        ses_data->buf_size = buf_size;
+        if (ses_data->thread_state != SES_THREAD_IDLE) {
+            pthread_mutex_unlock(&ses_data->lock);
+            agm_dbus_send_error(mdata->conn, msg, DBUS_ERROR_FAILED, "write via async failed");
+            return;
+        }
+        memcpy(ses_data->buf, value, n_elements);
+        ses_data->thread_state = SES_THREAD_WRITE_QUEUED;
+        snprintf(ses_data->eventType, sizeof("Signal"), "%s", "Signal");
         pthread_mutex_unlock(&ses_data->lock);
-        agm_dbus_send_error(mdata->conn, msg, DBUS_ERROR_FAILED, "write via async failed");
-        return;
+        async_thread_func(ses_data);
     }
-    memcpy(ses_data->buf, value, n_elements);
-    ses_data->thread_state = SES_THREAD_WRITE_QUEUED;
-    snprintf(ses_data->eventType, sizeof("Signal"), "%s", "Signal");
-    pthread_mutex_unlock(&ses_data->lock);
-    async_thread_func(ses_data);
+    else
+    {
+        ses_data->buf_size = buf_size;
+        memcpy(ses_data->buf, value, n_elements);
+        if (agm_session_write(ses_data->handle, ses_data->buf, (size_t *) &ses_data->buf_size)) {
+            AGM_LOGE("%s:agm_session_write failed\n", __func__);
+        }
+    }
 
     reply = dbus_message_new_method_return(msg);
     dbus_message_iter_init_append(reply, &r_arg);
