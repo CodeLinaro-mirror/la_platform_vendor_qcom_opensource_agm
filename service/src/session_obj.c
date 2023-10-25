ee54afeaba3796065b5d4240328c08dfe37ce3f9
Sandhya Mutha Naga Venkata <quic_smuthana@quicinc.com>
2023-10-25 14:07:04 +0530

    AGM: protect graph_read/graph_write with mutex
    graph_read/graph_write both should be protected with mutex since
    the sess_obj->graph can be freed in session_close()
    
    Change-Id: Ifcc0d4e4c2caced1bf23d07ede274c15029ba03f

--- service/src/session_obj.c
+++ service/src/session_obj.c
@@ -2644,16 +2644,16 @@ int session_obj_write_with_metadata(struct session_obj *sess_obj,
         AGM_LOGE("Cannot issue write in state:%d\n",
                             sess_obj->state);
         ret = -EINVAL;
-        pthread_mutex_unlock(&sess_obj->lock);
         goto done;
     }
-    pthread_mutex_unlock(&sess_obj->lock);
+
     ret = graph_write(sess_obj->graph, buffer, consumed_size);
     if (ret) {
         AGM_LOGE("Error:%d writing to graph\n", ret);
     }
 
 done:
+    pthread_mutex_unlock(&sess_obj->lock);
     return ret;
 }
 
@@ -2667,10 +2667,8 @@ int session_obj_read_with_metadata(struct session_obj *sess_obj,
         AGM_LOGE("Cannot issue read in state:%d\n",
                            sess_obj->state);
         ret = -EINVAL;
-        pthread_mutex_unlock(&sess_obj->lock);
         goto done;
     }
-    pthread_mutex_unlock(&sess_obj->lock);
 
     size_t read_size;
     ret = graph_read(sess_obj->graph, buffer, &read_size);
@@ -2680,7 +2678,9 @@ int session_obj_read_with_metadata(struct session_obj *sess_obj,
 
     *captured_size = (uint32_t)read_size;
 
+
 done:
+    pthread_mutex_unlock(&sess_obj->lock);
     return ret;
 }
 
