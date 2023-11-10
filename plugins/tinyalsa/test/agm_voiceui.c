b72932971588ca830097bda753dc7625d3a1b7ca
Harika Laveti <quic_hlaveti@quicinc.com>
2023-11-10 19:21:52 +0530

    AGM: Compilation error fixes
    1.Update agm_pcm_ioctl function prototype
    to align with tinyalsa's ioctl function.
    2.Update event_wait_thread_loop function prototype
    to align with kernel pthread_create function
    3.Issue was not seen earlier as
    incompatible-function-pointer-type errors
    are treated as warnings,which are now
    updated to errors.
    
    Change-Id: Ia9cae241c5abf2e8c7fba3890a20f890fe3f39c0
    (cherry picked from commit a22b945e50d1f8ca83b0c9799823f0800083fd96)
    (cherry picked from commit 0a5c772d934f0b45fc9f763923a640a55d88c326)

--- plugins/tinyalsa/test/agm_voiceui.c
+++ plugins/tinyalsa/test/agm_voiceui.c
@@ -28,7 +28,40 @@
 **
 ** Changes from Qualcomm Innovation Center are provided under the following license:
 ** Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+<<<<<<< HEAD   (471053 agm: Move ats thread creation after AGM init)
 ** SPDX-License-Identifier: BSD-3-Clause-Clear
+=======
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted (subject to the limitations in the
+** disclaimer below) provided that the following conditions are met:
+**
+**     * Redistributions of source code must retain the above copyright
+**       notice, this list of conditions and the following disclaimer.
+**
+**     * Redistributions in binary form must reproduce the above
+**       copyright notice, this list of conditions and the following
+**       disclaimer in the documentation and/or other materials provided
+**       with the distribution.
+**
+**     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+**       contributors may be used to endorse or promote products derived
+**       from this software without specific prior written permission.
+**
+** NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+** GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+** HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+** WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+** MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+** IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+** DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+** GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+** INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+** IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+** OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+** IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+>>>>>>> CHANGE (a22b94 AGM: Compilation error fixes)
 */
 
 #include <errno.h>
@@ -97,7 +130,7 @@ static void read_event_data(struct mixer *mixer, char *mixer_str)
     printf("%s params.event_payload_size %x\n", __func__, params->event_payload_size);
 }
 
-static void event_wait_thread_loop(void *context)
+static void *event_wait_thread_loop(void *context)
 {
     struct mixer *mixer = (struct mixer *)context;
     int ret = 0;
@@ -122,6 +155,7 @@ static void event_wait_thread_loop(void *context)
     }
 
     mixer_subscribe_events(mixer, 0);
+    return NULL;
 }
 
 static void record_lab_buffer(struct pcm *pcm, unsigned int cap_time)
