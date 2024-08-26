f3915bc9428744f561d0bfba074381d32ae70b2b
Qiang <quic_qianch@quicinc.com>
2024-08-26 15:44:23 +0800

    AGM: return error if AGM initialization fails
    return error if AGM initialization fails to avoid unexpected issues.
    ref change: Ib2ce3dfa5ecd3472d61bf6a01ca7878f9417286d
    
    Change-Id: I8180cc8fc619cfc252f36ff11f6ac0518e9850a4
    (cherry picked from commit c1a7e9ef6c38fcf94c4241dbdb3bf89ab364f85a)

--- ipc/aidl/server/Service.cpp
+++ ipc/aidl/server/Service.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
@@ -18,6 +18,10 @@ extern "C" __attribute__((visibility("default"))) binder_status_t registerServic
     auto agmService = ::ndk::SharedRefBase::make<AgmServerWrapper>();
     ndk::SpAIBinder agmBinder = agmService->asBinder();
     const std::string interfaceName = std::string() + IAGM::descriptor + "/default";
+    if (!agmService->isInitialized()) {
+        ALOGE("failed to initialize AGM Service!");
+        return -EINVAL;
+    }
 
     binder_status_t status = AServiceManager_addService(agmBinder.get(), interfaceName.c_str());
     ALOGI("register AGM Service interface %s registered %s ", interfaceName.c_str(),
