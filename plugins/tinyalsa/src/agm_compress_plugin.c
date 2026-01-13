8647defd92182471fd5db1fb64b312491f52a0d0
Radhika Garg <radhgarg@qti.qualcomm.com>
2026-01-12 22:34:25 -0800

    Update copyright and license marking.
    - Replaced "Qualcomm Innovation Center, Inc." with
      "Qualcomm Technologies, Inc. and/or its subsidiaries"
      across multiple source files.
    - Update SPDX identifier from BSD-3-Clause-Clear to BSD-3-Clause
    
    Change-Id: I2b13983d09c9e7a4f0bac97180992fb7321e0ae0
    Signed-off-by: Radhika Garg <radhgarg@qti.qualcomm.com>

--- plugins/tinyalsa/src/agm_compress_plugin.c
+++ plugins/tinyalsa/src/agm_compress_plugin.c
@@ -26,11 +26,11 @@
 ** OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 ** IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 **
- * Changes from Qualcomm Technologies, Inc. are provided under the following license:
- *
- * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
- *
+** Changes from Qualcomm Technologies, Inc. are provided under the following license:
+**
+** Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+** SPDX-License-Identifier: BSD-3-Clause
+**
 **/
 #define LOG_TAG "PLUGIN: compress"
 
