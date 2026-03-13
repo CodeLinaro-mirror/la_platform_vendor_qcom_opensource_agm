58166e15edf78815810d12877b50d78f355e5af8
Bhargavi Belagod Manjunath <bbelagod@qti.qualcomm.com>
2026-03-13 14:48:07 +0530

    agm: update shared memory endpoint mode for mmap usecases
    Signed-Of-By: Bhargavi B M<bbelagod@qti.qualcomm.com>
    Change-Id: Ibf27d16a6573683360c70bf135805f06d9ab1c7f

--- service/src/graph.c
+++ service/src/graph.c
@@ -146,7 +146,6 @@ done:
     return ret;
 }
 
-#define PULL_PUSH_SHMEM_ENDPOINT SHMEM_ENDPOINT
 int configure_buffer_params(struct graph_obj *gph_obj,
                             struct session_obj *sess_obj)
 {
@@ -243,7 +242,10 @@ int configure_buffer_params(struct graph_obj *gph_obj,
         buf_config.stop_threshold = sess_obj->stream_config.stop_threshold;
 
         if (mode == AGM_DATA_PUSH_PULL) {
-            buf_config.shmem_ep_tag = PULL_PUSH_SHMEM_ENDPOINT;
+	    if (sess_obj->stream_config.dir == TX)
+                 buf_config.shmem_ep_tag = SHMEM_PUSH_MODE;
+            else
+                 buf_config.shmem_ep_tag = SHMEM_PULL_MODE;
         } else {
             buf_config.shmem_ep_tag = SHMEM_ENDPOINT;
         }
