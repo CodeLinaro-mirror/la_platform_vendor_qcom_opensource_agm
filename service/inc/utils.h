fea32294675ba9e13edcae3d8b5d130f8ed1891d
Akshaya Chirikonda <achiriko@qti.qualcomm.com>
2026-05-19 10:15:20 +0530

    agm: copyright update
    Update audio-related OSS repositories to use the BSD-3-Clause license instead of BSD-3-Clause-Clear.
    Change-Id: I29daab66a6091657c428cc45b149698e75cf6ad3
    Signed-off-by: Akshaya Chirikonda <achiriko@qti.qualcomm.com>
    (cherry picked from commit d378f8a2fe05396647a33ab047ff8d63d524d55f)

--- service/inc/utils.h
+++ service/inc/utils.h
@@ -30,7 +30,7 @@
 /*
 ** Changes from Qualcomm Technologies, Inc. are provided under the following license:
 ** Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
-** SPDX-License-Identifier: BSD-3-Clause-Clear
+** SPDX-License-Identifier: BSD-3-Clause
 **
 ** Redistribution and use in source and binary forms, with or without
 ** modification, are permitted (subject to the limitations in the
